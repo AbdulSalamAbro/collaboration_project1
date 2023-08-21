f2fdbef76f6a102429bdab86cc4be02f85110139
Gideon <gideons564@gmail.com>
2023-08-21 05:39:33 +0100

    Add page two mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import "./App.css";
+import finetech from "./assets/images/fintech.png";
+import augmented_reality from "./assets/images/augmented_reality.png";
+import artificial_intelligence from "./assets/images/artificial_intelligence.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <section className="page-two">
+        
+        <div className="finetech">
+          <img src={finetech} alt="Fintech" />
+          <div className="content">
+            <h4>
+              How to optimize your pages to get more traffic on mobile devices
+            </h4>
+            <p>Read More &#x2192;</p>
+          </div>
+        </div>
+
+        <div className="augmented_reality">
+
+          <img src={augmented_reality} alt="Augmented Reality" />
+
+          <div className="content">
+            <h4>
+              How to optimize your pages to get more traffic on mobile devices
+            </h4>
+            <p>Read More &#x2192;</p>
+          </div>
+
+        </div>
+
+        <div className="artificial_intelligence">
+          <img src={artificial_intelligence} alt="Artificial Intelligence" />
+          <div className="content">
+            <h4>
+              How to optimize your pages to get more traffic on mobile devices
+            </h4>
+            <p>Read More &#x2192;</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
